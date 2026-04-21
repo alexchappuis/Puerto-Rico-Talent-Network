33dcd8e5e263b4ed1bb1c33b75b98b09e4a75298
Alex Chappuis <alexc26@bu.edu>
2026-04-21 15:32:30 -0400

    Run migrations and create superuser on deploy

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 python manage.py makemigrations website
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py createsuperuser --noinput
+python manage.py createsuperuser --noinput || true
