724967df94e5457d3bc6acab3c1ff97fa35a1e23
Alex Chappuis <alexc26@bu.edu>
2026-05-21 13:33:36 -0400

    Register submission models in admin

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import ProfessionalSubmission, CompanySubmission
 
-# Register your models here.
+
+@admin.register(ProfessionalSubmission)
+class ProfessionalSubmissionAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'location', 'field', 'submitted_at']
+    list_filter = ['field', 'submitted_at']
+    search_fields = ['first_name', 'last_name', 'email', 'location']
+    readonly_fields = ['submitted_at']
+    ordering = ['-submitted_at']
+
+
+@admin.register(CompanySubmission)
+class CompanySubmissionAdmin(admin.ModelAdmin):
+    list_display = ['company_name', 'contact_name', 'email', 'industry', 'size', 'submitted_at']
+    list_filter = ['industry', 'size', 'submitted_at']
+    search_fields = ['company_name', 'contact_name', 'email']
+    readonly_fields = ['submitted_at']
+    ordering = ['-submitted_at']
